45dbeb0701332130ae86f466fc89a8f9505b5ec0
Ewan Tindale <ewannnnn@gmail.com>
2020-09-24 15:33:44 +0100

    add basic form functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState("");
+
+  const submitForm = async (e) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: "/",
+        data: { numericString: input },
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h3>Phonewords Calculator</h3>
-        <form>
-          <input type="number" className="App-input" />
+        <form onSubmit={submitForm}>
+          <input
+            value={input}
+            onChange={handleInputChange}
+            type="number"
+            className="App-input"
+          />
           <div>
             <button type="submit" className="App-submit">
               Generate Words
@@ -17,6 +45,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
