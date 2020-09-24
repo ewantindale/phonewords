111f8a05e5e8bce19ff3238679bc0cccfcb3fdae
Ewan Tindale <ewannnnn@gmail.com>
2020-09-24 12:55:10 +0100

    add test route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const main = async () => {
   const app = express();
 
+  app.get("/", async (req, res) => {
+    res.send("Hello from Express");
+  });
+
   app.listen(4000, () => {
     console.log("Server is listening on http://localhost:4000");
   });
