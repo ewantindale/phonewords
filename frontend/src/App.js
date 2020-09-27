a79d63bd9c9229072fbece8625a1401a1f08c3b9
Ewan Tindale <ewannnnn@gmail.com>
2020-09-27 09:34:19 +0100

    implement numpad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,40 +13,56 @@ const App = () => {
   const loading = useSelector((state) => state.phonewords.loading);
   const error = useSelector((state) => state.phonewords.error);
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    dispatch(fetchResultsAsync(numericString, useWordFilter));
-  };
-
-  const handleInputChange = (e) => {
-    setNumericString(e.target.value);
-  };
-
   const handleFilterChange = () => {
     setUseWordFilter((useWordFilter) => !useWordFilter);
   };
 
-  return (
-    <div>
-      <header className={styles.header}>
-        <h1>Phonewords</h1>
-        <p className={styles.description}>
-          Enter a string containing only digits between 2 and 9
-        </p>
+  const numpadKeys = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+    10: "CLEAR",
+  };
 
-        <form onSubmit={handleFormSubmit}>
-          <div>
-            <input
-              value={numericString}
-              onChange={handleInputChange}
-              type="text"
-              className={styles.input}
-              placeholder="e.g. 23"
-            />
-            <button type="submit" className={styles.submit} disabled={loading}>
-              Generate Words
-            </button>
+  return (
+    <div className={styles.app}>
+      <h1>Phonewords</h1>
+      <div className={styles.inputWrapper}>
+        <div className={styles.input}>
+          <div className={styles.numericString}>{numericString}</div>
+          <div className={styles.error}>{error && error}</div>
+          <div className={styles.numpad}>
+            {Object.keys(numpadKeys).map((num) => (
+              <button
+                className={styles.button}
+                onClick={
+                  num < 10
+                    ? () => setNumericString((v) => v + num)
+                    : () => setNumericString("")
+                }
+                name={num}
+              >
+                {num < 10 ? <span className={styles.number}>{num}</span> : null}
+
+                <span className={styles.letters}>{numpadKeys[num]}</span>
+              </button>
+            ))}
           </div>
+          <button
+            type="submit"
+            className={styles.submit}
+            disabled={loading}
+            onClick={() =>
+              dispatch(fetchResultsAsync(numericString, useWordFilter))
+            }
+          >
+            Generate Words
+          </button>
           <div className={styles.filter}>
             <input
               type="checkbox"
@@ -56,28 +72,26 @@ const App = () => {
             />
             Filter by word list
           </div>
-        </form>
-
-        {error && <div className={styles.error}>Error: {error}</div>}
-
-        <div className={styles.resultsWrapper}>
-          <h2>Results: </h2>
-          {loading && (
-            <div className={styles.spinnerWrapper}>
-              <div className={styles.spinner}></div>
-            </div>
-          )}
-          {results && results.length > 0 && (
-            <div className={styles.results}>
-              {results.map((r, i) => (
-                <div key={i} className={styles.word}>
-                  {r}
-                </div>
-              ))}
-            </div>
-          )}
         </div>
-      </header>
+      </div>
+
+      <div className={styles.resultsWrapper}>
+        <h2>Results</h2>
+        {loading && (
+          <div className={styles.spinnerWrapper}>
+            <div className={styles.spinner}></div>
+          </div>
+        )}
+        {results && results.length > 0 && (
+          <div className={styles.results}>
+            {results.map((r, i) => (
+              <div key={i} className={styles.word}>
+                {r}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
